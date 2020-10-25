3e9ede29639841a311bfca261925caefdc3e0e70
meayasohn <meaya.sohn@gmail.com>
2020-10-25 13:04:26 -0700

    Updates

---
+++ precache-manifest.d69f9f6935b7133efacc41ee0ad17183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14004aa1909b573e5ae2c10ef01eea7",
+    "url": "/habit_tracker-app/index.html"
+  },
+  {
+    "revision": "a3f29c82735f26b2806a",
+    "url": "/habit_tracker-app/static/css/main.d7b31f83.chunk.css"
+  },
+  {
+    "revision": "91c27f55ef08e88d4098",
+    "url": "/habit_tracker-app/static/js/2.44ed3dbd.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit_tracker-app/static/js/2.44ed3dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f29c82735f26b2806a",
+    "url": "/habit_tracker-app/static/js/main.5fef37b6.chunk.js"
+  },
+  {
+    "revision": "892dc9d49f0fe324ff73",
+    "url": "/habit_tracker-app/static/js/runtime-main.45dbb3fd.js"
+  }
+]);
