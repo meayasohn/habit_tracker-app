fde3a9546647e43baddfbed4f1ccbdc69bd10c10
meayasohn <meaya.sohn@gmail.com>
2020-10-29 06:40:48 -0700

    Updates

---
+++ precache-manifest.b5a8f1ba7b61ed489680bb638baaf7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0228f66f1d9e69ed8e246af7228558e",
+    "url": "/habit_tracker-app/index.html"
+  },
+  {
+    "revision": "9565fbecf3d19ee6d73e",
+    "url": "/habit_tracker-app/static/css/main.bcdcf551.chunk.css"
+  },
+  {
+    "revision": "489c863070dd4d324cd7",
+    "url": "/habit_tracker-app/static/js/2.081865fd.chunk.js"
+  },
+  {
+    "revision": "76aa3a6712dcb2b7248a77b49e0cd963",
+    "url": "/habit_tracker-app/static/js/2.081865fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9565fbecf3d19ee6d73e",
+    "url": "/habit_tracker-app/static/js/main.0497618f.chunk.js"
+  },
+  {
+    "revision": "892dc9d49f0fe324ff73",
+    "url": "/habit_tracker-app/static/js/runtime-main.45dbb3fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/habit_tracker_app/precache-manifest.0b9ab26983a84e684bbc89b7bf7a170b.js"
+  "/habit_tracker-app/precache-manifest.b5a8f1ba7b61ed489680bb638baaf7da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/habit_tracker_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/habit_tracker-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
